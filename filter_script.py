75d57b039074dd97e19f027f1828e4454ec3ca74
Erin Shaw <erin.kimberly.shaw@gmail.com>
2018-02-23 12:35:56 -0500

    sprinkle in some generic error handling

--- filter_script.py
+++ filter_script.py
@@ -1,14 +1,17 @@
 import os, glob, re, sys
 
 def death_finder():
-  borough = sys.argv[1]
-  os.chdir(f'data/{borough}/')
-  os.mkdir(f'deaths')
+  try:
+    borough = sys.argv[1]
+    os.chdir(f'data/{borough}/')
+    os.mkdir(f'deaths')
 
-  for file in glob.glob('*.xlsx'):
-      death = re.search('death', file, re.IGNORECASE)
-      if death:
-        print(f'moving ye death file {file}')
-        os.rename(f'{file}', f'deaths/{file}')
+    for file in glob.glob('*.xlsx'):
+        death = re.search('death', file, re.IGNORECASE)
+        if death:
+          print(f'moving ye death file {file}')
+          os.rename(f'{file}', f'deaths/{file}')
+  except:
+    sys.exit(1)
 
 death_finder()
